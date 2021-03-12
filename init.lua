0765804d049f8b58dadfd67aa3469db4d2f39e9a
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2021-03-12 11:23:12 -0500

    use UI's theme

--- init.lua
+++ init.lua
@@ -2,18 +2,27 @@ invsaw = {}
 
 invsaw.users = {}
 
+local ui = unified_inventory
+
 invsaw.formspec = "formspec_version[4]"..
                   "size[15,15]"..
                   "background9[0,0;1,1;ui_formbg_9_sliced.png;true;16]"..
+                  "listcolors[#00000000;#FFFFFF80;#00000000]"..
+                  ui.single_slot(1.88, 0.88, true)..
                   "label[0.75,1.3;Input\nmaterial]" ..
                   "list[detached:invsaw_%s;input;2,1;1,1;]" ..
                   "label[0.75,3;Left-over]" ..
+                  ui.single_slot(1.88, 2.38)..
                   "list[detached:invsaw_%s;micro;2,2.5;1,1;]" ..
                   "label[0.75,4.3;Recycle\noutput]" ..
+                  ui.single_slot(1.88, 3.88)..
                   "list[detached:invsaw_%s;recycle;2,4;1,1;]" ..
                   "field[0.75,6;1,1;max_offered;Max:;%s]" ..
                   "button[2,6;1,1;Set;Set]" ..
+                  ui.make_inv_img_grid(3.88, 0.88, 8, 6)..
                   "list[detached:invsaw_%s;output;4,1;8,6;]" ..
+                  ui.make_inv_img_grid(3.88, 9.38,  8, 1, true)..
+                  ui.make_inv_img_grid(3.88, 10.63, 8, 3)..
                   "list[current_player;main;4,9.5;8,4;]"
 
 invsaw.nosawformspec =  "size[5,2]"..
